3a17754798fa4581bb225b473e89f6e70fca8abd
Cooper King <cking10@worcester.edu>
2019-04-28 23:45:21 -0400

    components part 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 
 import Menu from './components/Menu';
+import { DISHES } from './shared/dishes';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import './App.css';
 
 const App: React.FC = () => {
+
+  const [dishes, setDishes] = useState(DISHES);
+
   return (
     <div>
-      <Navbar>
+      <Navbar dark color="primary">
         <div className="container">
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
-      <Menu />>
+      <Menu dishes={dishes} />>
     </div>
   );
 }
