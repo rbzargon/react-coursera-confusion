831feda7b7670ae00cae649c80a1adbdcc9af81a
Cooper King <cking10@worcester.edu>
2019-05-08 21:40:08 -0400

    Add BrowserRouter to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Main from './components/Main';
 import './App.css';
+import {BrowserRouter} from 'react-router-dom';
 
 const App: React.FC = () => {
   return (
-    <Main />
+    <BrowserRouter>
+      <Main />
+    </BrowserRouter>
   );
 }
 
