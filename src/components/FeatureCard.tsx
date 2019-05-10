80f07c6f9f31b2478e08bb9ac12102ac4ad1bdd2
Cooper King <cking10@worcester.edu>
2019-05-09 23:56:22 -0400

    Add FeatureCard

---
+++ src/components/FeatureCard.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Card, CardBody, CardImg, CardText, CardTitle, CardSubtitle } from 'reactstrap';
+
+export interface IFeatureCardProps {
+    item: {
+        image: string,
+        name: string,
+        designation?: string,
+        description: string
+    }
+}
+
+export const FeatureCard: React.FC<IFeatureCardProps> = ({ item }) => {
+    return (
+        <Card>
+            <CardImg src={item.image} alt={item.name} />
+            <CardBody>
+                <CardTitle>{item.name}</CardTitle>
+                {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null}
+                <CardText>{item.description}</CardText>
+            </CardBody>
+        </Card>
+    );
+}
+
+export default FeatureCard;
