eb6ab6851e82f871b987602513cfcab9de131da7
Cooper King <cking10@worcester.edu>
2019-05-09 23:58:30 -0400

    Add Contact route Create application state Pass in featured items to HomePage

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,28 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
+import Contact from './Contact';
 import Footer from './Footer';
 import Header from './Header';
 import Home from './Home';
 import Menu from './Menu';
 import DishDetail from './DishDetail';
-import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { DISHES, IDish } from '../shared/dishes';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
 
-const HomePage = () => {
-    return (
-        <Home />
-    );
-}
+
+
+const MenuPage = React.memo(() => <Menu dishes={DISHES} />);
 
 export const Main: React.FC = () => {
 
-    const [dishes] = useState(DISHES);
+    const [state] = useState({
+        dishes: DISHES,
+        comments: COMMENTS,
+        promotions: PROMOTIONS,
+        leaders: LEADERS
+    });
+
+    const HomePage = () => {
+        return (
+            <Home dish={state.dishes.find(d => d.featured)}
+                promotion={state.promotions.find(p => p.featured)}
+                leader={state.leaders.find(l => l.featured)}/>
+        );
+    }
+
     return (
         <div>
             <Header />
             <Switch>
-                <Route path='/home' component={Home} />
-                <Route exact path='/menu' component={() => <Menu dishes={dishes} />} />
+                <Route path='/home' component={HomePage} />
+                <Route exact path='/menu' component={MenuPage} />
+                <Route exact path='/contactus' component={Contact} />>
                 <Redirect to='/home' />
             </Switch>
             <Footer />
