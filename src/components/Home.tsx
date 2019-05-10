4758bfd8941bbb9df23a722c51e39e763e34eefe
Cooper King <cking10@worcester.edu>
2019-05-09 23:57:23 -0400

    Add dish, promotion, leader feature cards to Home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,13 +1,37 @@
 import React from 'react';
 
-interface IHomeProps {
+import FeatureCard from './FeatureCard';
+import { IDish } from '../shared/dishes';
+import { IPromotion } from '../shared/promotions';
+import { ILeader } from '../shared/leaders';
 
+interface IHomeProps {
+    dish?: IDish,
+    promotion?: IPromotion,
+    leader?: ILeader,
 }
 
+
+
 export const Home: React.FC<IHomeProps> = (props) => {
+    const { dish, promotion, leader } = props;
+
     return (
         <div className="container">
-            <h4>Home</h4>
+            <div className="row align-items-start">
+                {!!dish ?
+                    <div className="col-12 col-md m-1">
+                        <FeatureCard item={dish} />
+                    </div> : ''}
+                {!!promotion ?
+                    <div className="col-12 col-md m-1">
+                        <FeatureCard item={promotion} />
+                    </div> : ''}
+                {!!leader ?
+                    <div className="col-12 col-md m-1">
+                        <FeatureCard item={leader} />
+                    </div> : ''}
+            </div>
         </div>
     )
 };
