9ba3c478aa4786aac504333715a99f169b2ad866
Cooper King <cking10@worcester.edu>
2019-07-01 22:43:22 -0400

    Create ADD_COMMENT action in redux.comments

--- src/redux/comments.tsx
+++ src/redux/comments.tsx
@@ -1,12 +1,23 @@
 import { COMMENTS } from '../shared/comments';
+import { ACTION_TYPE } from './actionType';
+import { CommentEntry } from './actionCreator';
 
 interface Action {
-    type: string
+    type: string,
+    payload: CommentEntry
 }
 
 export const Comments = (state = COMMENTS, action: Action) => {
     switch (action.type) {
+        case ACTION_TYPE.ADD_COMMENT: {
+            const commentEntry = {
+                ...action.payload,
+                id: state.length,
+                date: new Date(Date.now()).toLocaleString(navigator.language, { year: 'numeric', month: 'short', day: '2-digit' })
+        };
+            return state.concat(commentEntry);
+        }
         default:
-            return state
+            return state;
     }
 };
