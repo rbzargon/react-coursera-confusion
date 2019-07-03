eb4ec829194e6507ccdbc43bfed5356ff883edca
Cooper King <cking10@worcester.edu>
2019-07-03 01:10:35 -0400

    Add dishes action types

--- src/redux/actionType.tsx
+++ src/redux/actionType.tsx
@@ -1,4 +1,7 @@
 export const ACTION_TYPE = Object.freeze({
     ADD_COMMENT: 'ADD_COMMENT',
+    ADD_DISHES: 'ADD_DISHES',
+    DISHES_FAILED: 'DISHES_FAILED',
+    DISHES_LOADING: 'DISHES_LOADING',
 
 });
