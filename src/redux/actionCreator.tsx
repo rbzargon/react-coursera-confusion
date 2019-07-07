de3ca106bc2ee5c00ae83add76a602b9632ba3e3
Cooper King <cpking2@bu.edu>
2019-07-07 07:55:15 -0400

    Add dishes actions to redux.actionCreator

--- src/redux/actionCreator.tsx
+++ src/redux/actionCreator.tsx
@@ -1,38 +1,38 @@
 import { ACTION_TYPE } from './actionType';
 import { DISHES, Dish } from '../shared/dishes';
 import { Dispatch } from 'redux';
+import { DishesActionTypes } from './dishes';
 
 export interface CommentEntry {
-    dishId: number,
-    rating: number,
-    author: string,
-    comment: string
+    dishId: number;
+    rating: number;
+    author: string;
+    comment: string;
 }
 
-export const addComment = (entry: CommentEntry) => ({
+export const addComment = (commentEntry: CommentEntry): DishesActionTypes => ({
     type: ACTION_TYPE.ADD_COMMENT,
-    payload: entry
+    payload: { commentEntry },
 });
 
-export const fetchDishes = () => (dispatch: Dispatch) => {
-    dispatch(dishesLoading(true));
+export const addDishes = (dishes: Dish[]): DishesActionTypes => ({
+    type: ACTION_TYPE.ADD_DISHES,
+    payload: { dishes },
+});
 
-    setTimeout(() => {
+export const dishesFailed = (errorMessage: string): DishesActionTypes => ({
+    type: ACTION_TYPE.DISHES_FAILED,
+    payload: { errorMessage },
+});
+
+export const dishesLoading = (): DishesActionTypes => ({
+    type: ACTION_TYPE.DISHES_LOADING,
+});
+
+export const fetchDishes = (): Function => (dispatch: Dispatch): void => {
+    dispatch(dishesLoading());
+
+    setTimeout((): void => {
         dispatch(addDishes(DISHES));
     }, 2000);
 };
-
-export const dishesLoading = (isLoading: boolean) => ({
-    type: ACTION_TYPE.DISHES_LOADING,
-    payload: { isLoading }
-});
-
-export const dishesFailed = (errorMessage: string) => ({
-    type: ACTION_TYPE.DISHES_FAILED,
-    payload: { errorMessage }
-});
-
-export const addDishes = (dishes: Dish[]) => ({
-    type: ACTION_TYPE.ADD_DISHES,
-    payload: { dishes }
-});
