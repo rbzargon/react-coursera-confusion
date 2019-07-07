7863807337f81651fb0a91f85f6528d969851702
Cooper King <cking10@worcester.edu>
2019-07-07 09:11:48 -0400

    Change store comments type

--- src/redux/configureStore.tsx
+++ src/redux/configureStore.tsx
@@ -1,17 +1,17 @@
 import { AnyAction, applyMiddleware, combineReducers, createStore, Store } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
+import { Comment } from '../shared/comments';
 import { Leader } from '../shared/leaders';
 import { Promotion } from '../shared/promotions';
-import { CommentEntry } from './actionCreator';
 import { Comments } from './comments';
 import { dishesReducer, DishesState } from './dishes';
 import { Leaders } from './leaders';
 import { Promotions } from './promotions';
 
 export interface RootState {
-    comments: CommentEntry[];
-    dishes: DishesState;
+    comments: Comment[];
+    dishesState: DishesState;
     promotions: Promotion[];
     leaders: Leader[];
 }
@@ -20,7 +20,7 @@ export const configureStore = (): Store<RootState, AnyAction> => {
     const store: Store<RootState, AnyAction> = createStore(
         combineReducers({
             comments: Comments,
-            dishes: dishesReducer,
+            dishesState: dishesReducer,
             promotions: Promotions,
             leaders: Leaders,
         }),
