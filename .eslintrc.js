239fc36b65653a094374f0c1a0a0fc6bc7c6f5ff
Cooper King <cking10@worcester.edu>
2019-07-07 09:10:32 -0400

    Disable eslint explicit-function-return-type, react/prop-types

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,9 @@ module.exports = {
     rules: {
         // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
         // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+        "react/prop-types": "off",
+        "@typescript-eslint/explicit-function-return-type": "off",
+
     },
     settings: {
         react: {
