9335bc08db7cd54db5209cb577709be8270fcbef
Cooper King <cking10@worcester.edu>
2019-07-07 09:12:04 -0400

    Fix dishes state in main

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { connect } from "react-redux";
+import React, { FunctionComponentElement, FunctionComponent, ReactNode } from 'react';
+import { connect } from 'react-redux';
 import { Redirect, Route, RouteComponentProps, Switch, withRouter } from 'react-router-dom';
 import { Dispatch } from 'redux';
 import { AddCommentProvider } from '../context/addComment';
@@ -15,66 +15,76 @@ import Menu from './Menu';
 
 const mapStateToProps = (state: RootState) => {
     return state;
-}
+};
 
 interface DispatchFromProps {
-    addComment: (entry: CommentEntry) => void,
+    addComment: (entry: CommentEntry) => void;
 }
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({
-    addComment: (entry: CommentEntry) => dispatch(addComment(entry))
+    addComment: (entry: CommentEntry) => dispatch(addComment(entry)),
 });
 
-interface IDishWithIdProps {
+interface DishWithIdProps {
     match: {
         params: {
-            dishId: string
-        }
-    }
+            dishId: string;
+        };
+    };
 }
 
-
-export const Main: React.SFC<RootState & DispatchFromProps & RouteComponentProps> = (props) => {
-    console.log(props.comments);
-    const HomePage: React.SFC<void> = () => {
+export const Main: FunctionComponent<RootState & DispatchFromProps & RouteComponentProps> = props => {
+    const HomePage = (): FunctionComponentElement<void> => {
         return (
-            <Home dish={props.dishes.find(d => d.featured)}
+            <Home
+                dish={props.dishesState.dishes.find(d => d.featured)}
                 promotion={props.promotions.find(p => p.featured)}
-                leader={props.leaders.find(l => l.featured)} />
+                leader={props.leaders.find(l => l.featured)}
+            />
         );
     };
 
-    const MenuPage = React.memo(() => <Menu dishes={props.dishes} />);
+    const MenuPage = React.memo(() => <Menu dishes={props.dishesState.dishes} />);
+    MenuPage.displayName = 'MenuPage';
 
-    const DishWithId: React.SFC<IDishWithIdProps> = ({ match: { params: { dishId } } }) => {
+    const DishWithId: FunctionComponent<DishWithIdProps> = ({
+        match: {
+            params: { dishId },
+        },
+    }) => {
         return (
             <AddCommentProvider value={props.addComment}>
-                <DishDetail dish={props.dishes.find(d => d.id === parseInt(dishId))}
-                    comments={props.comments.filter(c => c.dishId === parseInt(dishId))} />
+                <DishDetail
+                    dish={props.dishesState.dishes.find(d => d.id === parseInt(dishId))}
+                    comments={props.comments.filter(c => c.dishId === parseInt(dishId))}
+                />
             </AddCommentProvider>
         );
     };
 
     const AboutUs: React.SFC<void> = () => {
-        return (
-            <About leaders={props.leaders} />
-        );
+        return <About leaders={props.leaders} />;
     };
 
     return (
         <div>
             <Header />
             <Switch>
-                <Route path='/home' component={HomePage} />
-                <Route exact path='/menu' component={MenuPage} />
-                <Route path='/menu/:dishId' component={DishWithId} />
-                <Route exact path='/contactus' component={Contact} />
-                <Route exact path='/aboutus' component={AboutUs} />
-                <Redirect to='/home' />
+                <Route path="/home" component={HomePage} />
+                <Route exact path="/menu" component={MenuPage} />
+                <Route path="/menu/:dishId" component={DishWithId} />
+                <Route exact path="/contactus" component={Contact} />
+                <Route exact path="/aboutus" component={AboutUs} />
+                <Redirect to="/home" />
             </Switch>
             <Footer />
         </div>
     );
-}
+};
 
-export default withRouter(connect<RootState, DispatchFromProps, {}, RootState>(mapStateToProps, mapDispatchToProps)(Main));
+export default withRouter(
+    connect<RootState, DispatchFromProps, {}, RootState>(
+        mapStateToProps,
+        mapDispatchToProps,
+    )(Main),
+);
