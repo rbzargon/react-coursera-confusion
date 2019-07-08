0a70e534dd836063e7ee7dd44c71437236c0be14
Cooper King <cking10@worcester.edu>
2019-07-07 22:49:38 -0400

    Format prettier

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import {
-    Card,
-    CardImg,
-    CardImgOverlay,
-    CardTitle
-} from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
 import { Dish } from '../shared/dishes';
 
-interface IMenuItemProps {
-    key: number,
-    dish: Dish,
-
+interface MenuItemProps {
+    key: number;
+    dish: Dish;
 }
 
-export const MenuItem: React.FC<IMenuItemProps> = ({ key, dish }) => {
+export const MenuItem: React.FC<MenuItemProps> = ({ key, dish }) => {
     return (
         <div key={key} className="col-12 col-md-5 m-1">
             <Card>
-                <Link to={`/menu/${dish.id}`} >
+                <Link to={`/menu/${dish.id}`}>
                     <CardImg width="100%" object src={dish.image} alt={dish.name} />
                     <CardImgOverlay>
                         <CardTitle>{dish.name}</CardTitle>
@@ -28,6 +22,6 @@ export const MenuItem: React.FC<IMenuItemProps> = ({ key, dish }) => {
             </Card>
         </div>
     );
-}
+};
 
 export default MenuItem;
