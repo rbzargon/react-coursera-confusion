6642d6588a800838d9bd6f7a4b40f722ca78f380
Cooper King <cking10@worcester.edu>
2019-07-08 21:07:38 -0400

    Format prettier

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,19 @@ import React from 'react';
 import Main from './components/Main';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from "react-redux";
-import { configureStore } from "./redux/configureStore";
+import { Provider } from 'react-redux';
+import { configureStore } from './redux/configureStore';
 
 const store = configureStore();
 
 const App: React.FC = () => {
-  return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Main />
-      </BrowserRouter>
-    </Provider>
-  );
-}
+    return (
+        <Provider store={store}>
+            <BrowserRouter>
+                <Main />
+            </BrowserRouter>
+        </Provider>
+    );
+};
 
 export default App;
