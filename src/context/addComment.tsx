b1a1e24e3d4461b4053d6eb1c2b8642e1074b5c9
Cooper King <cking10@worcester.edu>
2019-07-08 21:45:14 -0400

    Reformat

--- src/context/addComment.tsx
+++ src/context/addComment.tsx
@@ -1,21 +1,17 @@
 import React, { ReactNode } from 'react';
 import { CommentEntry } from '../redux/actionCreator';
 
-const AddCommentContext = React.createContext((entry: CommentEntry) => { });
+const AddCommentContext = React.createContext((entry: CommentEntry) => {});
 
 interface AddCommentProviderProps {
-    value: (entry: CommentEntry) => void,
-    children: ReactNode
+    value: (entry: CommentEntry) => void;
+    children: ReactNode;
 }
 
 const AddCommentProvider = (props: AddCommentProviderProps) => {
     const { value, children } = props;
 
-    return (
-        <AddCommentContext.Provider value={value}>
-            {children}
-        </AddCommentContext.Provider>
-    );
+    return <AddCommentContext.Provider value={value}>{children}</AddCommentContext.Provider>;
 };
 
 export { AddCommentContext, AddCommentProvider };
